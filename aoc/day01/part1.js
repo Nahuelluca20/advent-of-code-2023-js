b94347d1844521e5970a72537345609413fb371f
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-03 23:23:53 -0300

    remove console.log

--- aoc/day01/part1.js
+++ aoc/day01/part1.js
@@ -1,13 +1,13 @@
 const input = require("./resources/input")
 
+
+
 let sum = 0
 
 input.forEach((str) => {
   let num = 0
 
   num = str.match(/\d/g)
-
-  console.log(num[num.length - 1])
 
   sum = sum + Number(num[0] + num[num.length - 1])
 
